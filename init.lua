5f3d90742fe11ad6d84664ef319380bca88fa78b
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2025-03-07 12:13:20 +0100

    Init

---
+++ init.lua
@@ -0,0 +1,32 @@
+local core =
+  dofile(debug.getinfo(1, 'S').source:sub(2):match('(.*/)') .. 'core.lua')
+
+local getwinforfile = function(file)
+  for win in vis:windows() do
+    if win and win.file and win.file.path == file.path then
+      return win
+    end
+  end
+end
+
+local apply = function(win)
+  local options, syntax = core.get_pairs_for(win.file.lines)
+  for k, v in pairs(options) do
+    win.options[k] = v
+  end
+  if syntax then
+    win.syntax = syntax
+  end
+end
+
+vis.events.subscribe(vis.events.FILE_OPEN, function(file)
+  local win = getwinforfile(file)
+  if not win then
+    return
+  end
+  apply(win)
+end)
+
+vis.events.subscribe(vis.events.WIN_OPEN, function(win)
+  apply(win)
+end)
